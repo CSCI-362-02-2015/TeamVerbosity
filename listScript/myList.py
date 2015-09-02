99dfa355cd475b2638af9c0a9994cd07761854c0
BloodyHoney <jstc.14@gmail.com>
2015-09-02 13:22:28 -0400

    Next time: Create new branch before merge

--- listScript/myList.py
+++ listScript/myList.py
@@ -1,9 +1,20 @@
 #! /usr/bin/env python
 
-import subprocess
+import subprocess, webbrowser, sys
 
 myList = open("dir.html", 'w')
+if sys.platform == "win32":
+	com = 'dir'
+else:
+	com = 'ls'
+
 myList.write("<html>\n")
-myList.write(subprocess.check_output(["ls"]))
-myList.write("</html>")
+myStr = (subprocess.check_output([com]))
+for s in (myStr.split()):
+	myList.write(s + "<br>")
+myList.write("\n</html>")
 myList.close()
+
+webbrowser.open("dir.html")
+sys.exit()
+print "Did not close"
